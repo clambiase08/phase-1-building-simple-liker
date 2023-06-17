f368cf8e366254eeb247f094178c0ca6e7435adb
Christina Lambiase <christina.lambiase@gmail.com>
2023-06-16 17:44:08 -0700

    complete lab

--- main.js
+++ main.js
@@ -3,11 +3,63 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const divSelect = document.querySelector("div")
+const hearts = document.querySelectorAll('.like-glyph');
+
+function removeError(){
+  divSelect.classList.add("hidden")
+}
+
+function handleClick(heart) {
+  mimicServerCall()
+  .then(() => {
+    heart.textContent = FULL_HEART
+    heart.classList.add("activated-heart")
+  })
+  .catch(() => {
+    divSelect.classList.remove("hidden")
+    setTimeout(removeError, 3000) 
+  })
+}
+
+function handleFullHeart(heart) {
+    if (heart.classList.contains("activated-heart")) {
+    heart.textContent = EMPTY_HEART  
+    heart.classList.remove("activated-heart")
+    }
+  }
 
 
-
-
-//------------------------------------------------------------------------------
+hearts.forEach((heart) => {
+  heart.addEventListener("click", ()=> {
+  if (heart.textContent === EMPTY_HEART) {
+      handleClick(heart)
+    } else if (heart.textContent === FULL_HEART) {
+      handleFullHeart(heart)
+    }
+  })
+})
+  
+  // hearts.forEach((heart) => {
+  //   heart.addEventListener("click", () => {
+  //     if (heart.textContent === EMPTY_HEART) {
+  //       mimicServerCall()
+  //       .then(() => {
+  //         heart.textContent = FULL_HEART
+  //         heart.classList.add("activated-heart")
+  //       })
+  //       .catch(() => {
+  //         divSelect.classList.remove("hidden")
+  //         setTimeout(() => divSelect.classList.add("hidden"), 3000) 
+  //       })
+  //     } else if (heart.textContent === FULL_HEART) {
+  //       heart.textContent = EMPTY_HEART
+  //       heart.classList.remove("activated-heart")
+  //     }
+  //   })
+  // })
+  
+  //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
